2ec77c4a63127e5712a98857bf5eaf96b7b0031e
mcthoren <thoren.mcdole@gmail.com>
2017-01-30 19:43:20 +0000

    i think this turns off that big blink bar if you press some f key

--- user.js
+++ user.js
@@ -6,3 +6,4 @@ user_pref("noscript.notify", false);
 user_pref("noscript.notify.bottom", false);
 user_pref("noscript.showAddress", true);
 user_pref("noscript.untrusted", "adobedtm.com doubleclick.net facebook.net googl-analytics.com google-analytics.com googleadservices.com ioam.de media.net outbrain.com taboola.com http://adobedtm.com http://doubleclick.net http://facebook.net http://googl-analytics.com http://google-analytics.com http://googleadservices.com http://ioam.de http://media.net http://outbrain.com http://taboola.com https://adobedtm.com https://doubleclick.net https://facebook.net https://googl-analytics.com https://google-analytics.com https://googleadservices.com https://ioam.de https://media.net https://outbrain.com https://taboola.com");
+user_pref("accessibility.typeaheadfind.flashBar", 0);
